e975c5bc72f30c7bfa106fd3dc912a6d32b26ccb
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-20 18:30:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -25,3 +25,8 @@ def first_element_with_array_methods(south_east_asia)
    ["Thailand", "Cambodia", "Singapore", "Myanmar"]
    @south_east_asia.first
 end
+
+def last_element_with_array_methods(south_east_asia)
+   ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+   @south_east_asia.last 
+end
