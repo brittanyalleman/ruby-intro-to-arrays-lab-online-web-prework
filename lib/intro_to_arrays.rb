cb71cbf2165a13704dcb5b3b357024de8fc65bf1
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-20 18:14:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,4 +9,9 @@ end
 def first_element(taylor_swift)
  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   @taylor_swift[0]
-  end
+end
+
+def third_element(taylor_swift)
+   ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    @taylor_swift[2]
+end
