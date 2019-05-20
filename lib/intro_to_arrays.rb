ee7709aaf5d55d08772c494151f7ed04e97e35a1
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-20 18:03:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,8 +6,8 @@ def array_with_two_elements
   ["january", "February"]
 end
 
-def first_element
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-taylor_swift[0]
+def first_element= first_element(@taylor_swift)
+ ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[0]
   end
 end
