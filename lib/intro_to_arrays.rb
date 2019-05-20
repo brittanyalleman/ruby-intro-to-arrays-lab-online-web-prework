91d02e8f853dd24212a4394f002d60d712399acb
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-20 17:53:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,6 +7,6 @@ def array_with_two_elements
 end
 
 def first_element
-  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   @taylor_swift[0]
 end
